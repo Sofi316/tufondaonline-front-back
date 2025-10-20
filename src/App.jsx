e63241cb9b8fe677d287da7f2a4f325ecc2f4d60
fasanhueza <fa.sanhuezuezao@duocuc.cl>
2025-10-20 13:03:48 -0300

    Resolviendo problemas

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import Footer from './components/Footer';
-import MiFoto from './components/MiFoto';
 import Registro from './pages/Registro';
 import Productos from './pages/Productos';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './css/stylesheet.css';
+import './styles/styles.css'
 
 
 export default function App() {
@@ -25,9 +22,6 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/mifoto" element={<MiFoto />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/productos" element={<Productos />} />
           <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
